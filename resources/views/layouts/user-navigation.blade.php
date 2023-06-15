feaada6b42bd9f109a0efe233e13bb23ecd4a36a
Toru959 <80747436+Toru959@users.noreply.github.com>
2023-06-15 15:26:28 +0800

    index途中

--- resources/views/layouts/user-navigation.blade.php
+++ resources/views/layouts/user-navigation.blade.php
@@ -18,6 +18,93 @@
                 </div>
             </div>
 
+            {{-- <header class="relative flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white text-sm py-4 "> --}}
+                <nav class="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
+                  {{-- <div class="flex items-center justify-between">
+                    <div class="sm:hidden">
+                      <button type="button" class="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800" data-hs-collapse="#navbar-collapse-basic" aria-controls="navbar-collapse-basic" aria-label="Toggle navigation">
+                        <svg class="hs-collapse-open:hidden w-4 h-4" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                          <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                        </svg>
+                        <svg class="hs-collapse-open:block hidden w-4 h-4" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                          <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                        </svg>
+                      </button>
+                    </div>
+                  </div> --}}
+
+                  <div id="navbar-collapse-basic" class="hidden basis-full grow sm:block">
+                    <div class="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:pl-5">
+                        <div>
+                            <label for="hs-trailing-button-add-on-with-icon-and-button" class="sr-only">Label</label>
+                            <div class="relative flex rounded-md shadow-sm">
+                              <input type="text" id="hs-trailing-button-add-on-with-icon-and-button" name="hs-trailing-button-add-on-with-icon-and-button" class="py-3 px-4 pl-11 block w-full border-gray-200 shadow-sm rounded-l-md text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-5 dark:text-gray-400">
+                              <div class="absolute inset-y-0 left-0 flex items-center pointer-events-none z-20 pl-4">
+                                <svg class="h-4 w-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                                </svg>
+                              </div>
+                              <button type="button" class="py-3 px-4 inline-flex flex-shrink-0 justify-center items-center rounded-r-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:z-10 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all text-sm">Search</button>
+                            </div>
+                        </div>
+
+                        <a class="font-medium text-gray-400 dark:hover:text-gray-500" href="#" aria-current="page">Create</a>
+
+                        <!-- Settings Dropdown -->
+                        <div class="hidden sm:flex sm:items-center sm:ml-6">
+                            <x-dropdown align="right" width="48">
+                                <x-slot name="trigger">
+                                    <a class="text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500 flex items-center text-sm font-medium hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out" href="#" aria-current="page">
+                                        <button class="text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500 flex items-center text-sm font-medium hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                                            Categories
+                                            <div class="ml-1">
+                                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                                </svg>
+                                            </div>
+                                        </button>
+                                    </a>
+                                </x-slot>
+
+                                <x-slot name="content">
+                                    <!-- Authentication -->
+                                    {{-- <form method="POST" action="{{ route('user.logout') }}">
+                                        @csrf --}}
+
+                                        <x-dropdown-link>
+                                                {{-- onclick="event.preventDefault();
+                                                            this.closest('form').submit();">
+                                            {{ __('Log Out') }} --}}
+                                            {{-- <div class="hs-dropdown-menu transition-[opacity,margin] duration-[0.1ms] sm:duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 sm:w-48 hidden z-10 bg-white sm:shadow-md rounded-lg p-2 dark:bg-white sm:dark:border dark:border-gray-700 dark:divide-gray-700 before:absolute top-full sm:border before:-top-5 before:left-0 before:w-full before:h-5"> --}}
+                                                <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-white focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-white dark:hover:text-gray-300" href="#">
+                                                Business
+                                                </a>
+                                                <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-white focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-white dark:hover:text-gray-300" href="#">
+                                                Hobby
+                                                </a>
+                                                <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-white focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-white dark:hover:text-gray-300" href="#">
+                                                Study
+                                                </a>
+                                                <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-white focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-white dark:hover:text-gray-300" href="#">
+                                                Trade
+                                                </a>
+                                                <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-white focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-white dark:hover:text-gray-300" href="#">
+                                                Other
+                                                </a>
+                                            {{-- </div> --}}
+                                        </x-dropdown-link>
+                                    {{-- </form> --}}
+                                </x-slot>
+                            </x-dropdown>
+                        </div>
+            
+                      <a class="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500" href="#">English</a>
+                    </div>
+                  </div>
+                  
+                </nav>
+            {{-- </header> --}}
+
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <x-dropdown align="right" width="48">
