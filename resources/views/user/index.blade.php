8f2bf77d02bb1e7ba700a16f55d5e74a307091b1
Toru959 <80747436+Toru959@users.noreply.github.com>
2023-06-15 13:47:12 +0800

    index途中

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,57 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            一覧ページ
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <section class="text-gray-600 body-font">
+                    <div class="container px-5 mx-auto">
+                        <div class="flex flex-wrap w-full mb-20">
+                        <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
+                            <div class="sm:text-1xl text-1xl font-medium title-font mb-2 text-gray-900">New Event -></div>
+                        </div>
+                        <div class="flex flex-wrap -m-4">
+                        <div class="xl:w-1/4 md:w-1/2 p-4">
+                            <div class="bg-gray-100 p-6 rounded-lg">
+                            <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content">
+                            <h2 class="text-lg text-gray-900 font-medium title-font mb-4 text-center">Event Title</h2>
+                            <p class="leading-relaxed text-base">Laocation: text text text</p>
+                            <p class="leading-relaxed text-base">Date: text text text</p>
+                            </div>
+                        </div>
+                        <div class="xl:w-1/4 md:w-1/2 p-4">
+                            <div class="bg-gray-100 p-6 rounded-lg">
+                            <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content">
+                            <h2 class="text-lg text-gray-900 font-medium title-font mb-4 text-center">Event Title</h2>
+                            <p class="leading-relaxed text-base">Laocation: text text text</p>
+                            <p class="leading-relaxed text-base">Date: text text text</p>
+                            </div>
+                        </div>
+                        <div class="xl:w-1/4 md:w-1/2 p-4">
+                            <div class="bg-gray-100 p-6 rounded-lg">
+                            <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content">
+                            <h2 class="text-lg text-gray-900 font-medium title-font mb-4 text-center">Event Title</h2>
+                            <p class="leading-relaxed text-base">Laocation: text text text</p>
+                            <p class="leading-relaxed text-base">Date: text text text</p>
+                            </div>
+                        </div>
+                        <div class="xl:w-1/4 md:w-1/2 p-4">
+                            <div class="bg-gray-100 p-6 rounded-lg">
+                            <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content">
+                            <h2 class="text-lg text-gray-900 font-medium title-font mb-4 text-center">Event Title</h2>
+                            <p class="leading-relaxed text-base">Laocation: text text text</p>
+                            <p class="leading-relaxed text-base">Date: text text text</p>
+                            </div>
+                        </div>
+                    </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
