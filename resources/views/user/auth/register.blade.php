e4c34f3341af57ac11477867b7eff8494366d04e
Hiromu1663 <132648643+Hiromu1663@users.noreply.github.com>
2023-06-20 23:26:23 +0800

    age→birthdayへ登録内容変更

--- resources/views/user/auth/register.blade.php
+++ resources/views/user/auth/register.blade.php
@@ -33,25 +33,80 @@
             </div>
             
 
-            <!--Gender-->
+            
             <div class="mt-4 flex">
-                <div class="mr-4">
+                <!--Gender-->
+                <div class="mt-4">
                     <x-label for="gender" :value="__('Gender')" />
-                    <div id="gender" class="flex">
-                        <x-label for="male" :value="__('Male')" />
-                        <x-input id="male" class="block mt-1 mr-2" type="radio" name="gender" value="Male" />
-                        <x-label for="female" :value="__('Female')" />
-                        <x-input id="female" class="block mt-1 mr-2" type="radio" name="gender" value="Female" />
+                    <div id="gender" class="flex flex-col mr-4">
+                        <div class="flex">
+                            <x-label for="male" :value="__('Male')" />
+                            <x-input id="male" class="block mt-1 mr-2" type="radio" name="gender" value="Male" />
+                        </div>
+                        <div class="flex">
+                            <x-label for="female" :value="__('Female')" />
+                            <x-input id="female" class="block mt-1 mr-2" type="radio" name="gender" value="Female" />
+                        </div>
                     </div>
-                </div>            
-    
-                <!--age-->
-                <div class="">
-                    <x-label for="age" :value="__('Age')" />
-                    <x-input id="age" class="block h-6" type="number" name="age" min="18" max="100" :value="old('age')" required />
-                </div>
+                </div> 
 
+                <!--status-->
+                <div class="mt-4">
+                    <x-label for="status" :value="__('Status')" />
+                    <select id="status" name="status" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block">
+                        <option value="">----</option>
+                        <option value="Employee">Employee</option>
+                        <option value="Civil Servant">Civil Servant</option>
+                        <option value="Self-employed">Self-employed</option>
+                        <option value="Student">Student</option>
+                        <option value="Artist">Artist</option>
+                        <option value="Doctor">Doctor</option>
+                        <option value="Lawyer">Lawyer</option>
+                        <option value="Teacher">Teacher</option>
+                        <option value="Engineer">Engineer</option>
+                        <option value="Salesperson">Salesperson</option>
+                        <option value="Other">Other</option>
+                    </select>
+                </div>
             </div>
+
+         
+
+            {{-- birthday --}}
+            <div class="mt-4">
+                <x-label for="birthday" :value="__('Birthday')" />
+                <div id='birthday' class="flex">
+                    {{-- year --}}
+                    <div class="mr-4">
+                        <select id="birth_year" name="birth_year" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block">
+                        <option value="">Year</option>
+                        @for ($year = 1920; $year <= date('Y'); $year++)
+                            <option value="{{ $year }}">{{ $year }}</option>
+                        @endfor
+                        </select>
+                    </div>
+                    {{-- month --}}
+                    <div class="mr-4">
+                        <select id="birth_month" name="birth_month" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block">
+                        <option value="">Month</option>
+                        @for ($month = 1; $month <= 12; $month++)
+                            <option value="{{ $month }}">{{ $month }}</option>
+                        @endfor
+                        </select>
+                    </div>
+                    {{-- day --}}
+                    <div class="mr-4">
+                        <select id="birth_day" name="birth_day" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block">
+                        <option value="">Day</option>
+                        @for ($day = 1; $day <= 31; $day++)
+                            <option value="{{ $day }}">{{ $day }}</option>
+                        @endfor
+                        </select>
+                    </div>
+                </div>
+            </div>
+            
+        
             
             
 
@@ -68,25 +123,6 @@
                 <x-input id="avatar" class="block mt-1 w-full" type="file" name="avatar" :value="old('avatar')" />
             </div>
 
-             <!--status-->
-            <!--status-->
-            <div class="mt-4">
-                <x-label for="status" :value="__('Status')" />
-                <select id="status" name="status" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block">
-                    <option value="">職業を選択してください</option>
-                    <option value="Employee">Employee</option>
-                    <option value="Civil Servant">Civil Servant</option>
-                    <option value="Self-employed">Self-employed</option>
-                    <option value="Student">Student</option>
-                    <option value="Artist">Artist</option>
-                    <option value="Doctor">Doctor</option>
-                    <option value="Lawyer">Lawyer</option>
-                    <option value="Teacher">Teacher</option>
-                    <option value="Engineer">Engineer</option>
-                    <option value="Salesperson">Salesperson</option>
-                    <option value="Other">Other</option>
-                </select>
-            </div>
 
             
             <!-- Password -->
